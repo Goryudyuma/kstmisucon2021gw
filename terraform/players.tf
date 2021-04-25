bb3b90af8866257ecf73d8236dd5175079d15974
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2021-04-26 00:47:24 +0900

    参加者の追加

--- terraform/players.tf
+++ terraform/players.tf
@@ -1,6 +1,16 @@
 locals {
   players = toset([
     "Goryudyuma",
-    "bgpat"
+    "bgpat",
+    "passerby-1",
+    "kitakou0313",
+    "hirominn",
+    "togacoder",
+    "arsley",
+    "zipnofastener",
+    "Noiri",
+    "TOY-toranoko",
+    "ritsuxis",
+    "kitoriaaa"
   ])
 }
